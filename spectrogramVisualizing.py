7982c9d9421e9577372262a283b0f92ef28cc460
betegon <miguelbetegongarcia@gmail.com>
2020-03-08 21:06:48 +0100

    Add padding to last window of time_series for STFT

--- spectrogramVisualizing.py
+++ spectrogramVisualizing.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+# TODO: Document That I have used zero padding for STFT
+# TODO: Change values in args.py to get 16064Hz as frame rate  and all the other parameters.
+# TODO: Rename audio_to_audio_frame_stack() to time_series_to_frame_stack()
+# TODO: Substitute the old functions by the ones here.
+# TODO: Handle different encoding types of audio + channels (2 vs 1 channel).
+#       Best way is maybe convert from 2 to 1 channel.
+
 
 def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length, hop_length_frame, min_duration):
     """This function take audio files of a directory and merge them
@@ -20,14 +27,17 @@ def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length,
 
         if (total_duration >= min_duration):
             list_sound_array.append(
-                audio_to_audio_frame_stack(y, frame_length, hop_length_frame))
+                audio_to_audio_frame_stack(
+                    sound_data=y,
+                    frame_length=frame_length,
+                    hop_length_frame=hop_length_frame))
             print(list_sound_array)
         else:
             list_audio_below_min_duration.append(os.path.join(audio_dir, file))
             print("The following file {} is below the min duration".format(os.path.join(audio_dir, file)))
 
     if list_audio_below_min_duration:
-        print("These are the files below the minimum duration ({} seconds): ".format(min_duration))
+        print("Following files are below minimum duration ({} seconds) and won't be included: ".format(min_duration))
         for audio_below_duration in list_audio_below_min_duration:
             print("  {}".format(audio_below_duration))
 
@@ -42,17 +52,41 @@ def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length,
         return
 
 
-def audio_to_audio_frame_stack(sound_data, frame_length, hop_length_frame):
+def audio_to_audio_frame_stack(sound_data, frame_length, hop_length_frame, include_if_bigger_than=0.2):
     """This function take an audio and split into several frames
-       in a numpy matrix of size (nb_frame,frame_length)"""
+       in a numpy matrix of size (nb_frame,frame_length)
 
-    sequence_sample_length = sound_data.shape[0]
+    args:
+        hop_length_frame (int): Sliding window.
+        include_if_bigger_than (float): Value between 0 and 1. Default to 0.2. Include last window (that will be padded) if it is greater than a percentage of the sliding window.
 
-    sound_data_list = [sound_data[start:start + frame_length] for start in range(
-    0, sequence_sample_length - frame_length + 1, hop_length_frame)]  # get sliding windows
+       note: to match window size (i.e hop_length_frame), it applies a zero padding.
+
+    """
+    # sequence_sample_length = sound_data.shape[0]
+    # sound_data_list = [sound_data[start:(start + frame_length)] for start in range(0, sequence_sample_length - frame_length + 1, hop_length_frame)]  # get sliding windows
+    # sound_data_array = np.vstack(sound_data_list)
+    # return sound_data_array
+
+    time_series_length = sound_data.shape[0]
+    sound_data_list = []
+    for start in range(0, time_series_length, hop_length_frame):
+        frame = sound_data[start:(start + frame_length)]
+        if(frame.shape[0] == hop_length_frame):
+            sound_data_list.append(frame)
+        elif(frame.shape[0] < hop_length_frame and
+                frame.shape[0] > (include_if_bigger_than * hop_length_frame)):
+            # if it is the last element, add zero padding to match hop_length_frame
+            print(frame.shape)
+            frame = np.pad(frame, (0, hop_length_frame-frame.shape[0]), 'constant')
+            print("longitud de frame despues del padding")
+            print(frame)
+
+        print(type(frame))
+        print(frame.shape)
+
     sound_data_array = np.vstack(sound_data_list)
-
-    return sound_data_array
+    print(sound_data_array.shape)
 
 
 def save_audio(y, sample_rate, output_name='audio_ouput.wav'):
@@ -92,8 +126,6 @@ y_clean, sr_clean = load(os.path.join(audio_dir, clean), sr=sample_rate)
 y_mixed, sr_mixed = load(os.path.join(audio_dir, mixed), sr=sample_rate)
 y_noise, sr_noise = load(os.path.join(audio_dir, noise), sr=sample_rate)
 
-save_audio(y_clean, sample_rate, 'mispe.wav')
-
 # Total duration = y/sr
 total_duration_clean = get_duration(y=y_clean, sr=sr_clean)
 total_duration_mixed = get_duration(y=y_mixed, sr=sr_mixed)
@@ -102,9 +134,60 @@ print("Duration clean: {} seconds.".format(total_duration_clean))
 print("Duration mixed: {} seconds.".format(total_duration_mixed))
 print("Duration noise: {} seconds.".format(total_duration_noise))
 
+# PLOT TIME SERIES
 time_series = [y_clean, y_mixed, y_noise, y_mixed - y_noise]
 titles = ['Clean voice', 'Mixed voice', 'Noise', 'Mixed voice - Noise = Clean voice']
 plot_time_series(time_series, titles)
 
+''''STEPS FOR CREATING DATASET
+1. Audio files to numpy (audio_files())
+    1.1. Load .wav
+    1.2. audio to audio frame stack (audio_to_audio_frame_stack())
+    1.3. Append result from 1.2 to audio list (list_sound_array)
+2. Blend noise randomly (blend_noise_randomly())
+3. noisy_voice_long = reshape
+4. save noisy_voice_long
+5. Repeat 3. & 4. for voice_long and noise_long
+'''
 
+# 1. Audio files to numpy
 audio_files_to_numpy(audio_dir, [clean, mixed, noise], sample_rate, frame_length, hop_length_frame, min_duration)
+
+# 1.2 Audio to audio frame stack
+list_sound_array = []
+appending = audio_to_audio_frame_stack(y_clean, frame_length, hop_length_frame)
+print("\ny_clean \nappending.\n")
+print(y_clean)
+print(appending)
+print(y_clean.shape)
+if appending:
+    print(appending.shape)
+# 1.3 Append result from 1.2 to list_sound_array
+list_sound_array.append(appending)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Example of saving audio
+save_audio(y_clean, sample_rate, 'mispe.wav')
