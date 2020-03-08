ea6f88b9077e97c0c852d0c1f9af36ed15abec1d
betegon <miguelbetegongarcia@gmail.com>
2020-03-08 22:22:31 +0100

    Style doc

--- spectrogramVisualizing.py
+++ spectrogramVisualizing.py
@@ -1,5 +1,4 @@
-from librosa.output import write_wav
-from librosa import load, get_duration
+import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -14,24 +13,28 @@ import os
 
 def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length, hop_length_frame, min_duration):
     """This function take audio files of a directory and merge them
-    in a numpy matrix of size (nb_frame,frame_length)
+    in a numpy matrix of size (total_frames,frame_length)
     for a sliding window of size hop_length_frame
 
-    args:
+    Args:
         audio_dir (str): Directory where audios are located
         list_audio_files (list): string list of names of audio files.
         sample_rate (int): Sample rate of audios.
         frame_length (int): Length of frames.
         min_duration (int): Mininum duration of the audios to be added.
+
+    Returns:
+        np.ndarray: numpy ndarray shape (total_frames, frame_length).
     """
 
     list_sound_array = []
     list_audio_below_min_duration = []
 
+    print("Total files to be processed: {}".format(len(list_audio_files)))
     for file in list_audio_files:
         # open the audio file
-        y, sr = load(os.path.join(audio_dir, file), sr=sample_rate)
-        total_duration = get_duration(y=y, sr=sr)
+        y, sr = librosa.load(os.path.join(audio_dir, file), sr=sample_rate)
+        total_duration = librosa.get_duration(y=y, sr=sr)
 
         if (total_duration >= min_duration):
             list_sound_array.append(
@@ -39,7 +42,7 @@ def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length,
                     sound_data=y,
                     frame_length=frame_length,
                     hop_length_frame=hop_length_frame))
-            print(list_sound_array)
+
         else:
             list_audio_below_min_duration.append(os.path.join(audio_dir, file))
             print("The following file {} is below the min duration".format(os.path.join(audio_dir, file)))
@@ -51,27 +54,28 @@ def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length,
 
     if list_sound_array:
         print("Lista de arrays: \n\n")
-        print(list_sound_array)
+        print(list_sound_array[0].shape)
         print("\n\n\n\nLista de arrays ESTAKADA: \n\n")
-        print(np.vstack(list_sound_array))
+        stacked = np.vstack(list_sound_array)
+        print(type(stacked))
         return np.vstack(list_sound_array)
     else:
         print("There aren't any files above minimum duration ({} seconds).".format(min_duration))
-        return
 
 
 def audio_to_audio_frame_stack(sound_data, frame_length, hop_length_frame, include_if_bigger_than=0.2):
     """This function take an audio and split into several frames
-       in a numpy matrix of size (nb_frame,frame_length)
+       in a numpy matrix of size (number_of_frames,frame_length).
 
-    args:
+    Args:
         sound_data (list): List of amplitudes returned from librosa.load().
         frame_length (int): Length of frames.
         hop_length_frame (int): Sliding window.
         include_if_bigger_than (float): Value between 0 and 1. Default to 0.2. Include last window (that will be padded) if it is greater than a percentage of the sliding window.
-
+    Returns:
+        np.ndarray: Multidimensional array of shape (number_of_frames, frame_length).
+                    I
     note: to match window size (i.e hop_length_frame), it applies a zero padding.
-
     """
 
     sound_data_list = []
@@ -93,7 +97,7 @@ def save_audio(y, sample_rate, output_name='audio_ouput.wav'):
     Save audio file given y (amplitude values) and sample_rate.
     By default, output name is 'audio_output.wav'
     '''
-    write_wav(output_name, y_mixed - y_noise, sample_rate)
+    librosa.output.write_wav(output_name, y_mixed - y_noise, sample_rate)
 
 
 def plot_time_series(time_series_list, time_series_titles):
@@ -121,14 +125,14 @@ frame_length = sample_rate + 64  # a bit more than sample_rate for avoiding over
 min_duration = 1  # Seconds
 hop_length_frame = sample_rate + 64
 
-y_clean, sr_clean = load(os.path.join(audio_dir, clean_filename), sr=sample_rate)
-y_mixed, sr_mixed = load(os.path.join(audio_dir, mixed_filename), sr=sample_rate)
-y_noise, sr_noise = load(os.path.join(audio_dir, noise_filename), sr=sample_rate)
+y_clean, sr_clean = librosa.load(os.path.join(audio_dir, clean_filename), sr=sample_rate)
+y_mixed, sr_mixed = librosa.load(os.path.join(audio_dir, mixed_filename), sr=sample_rate)
+y_noise, sr_noise = librosa.load(os.path.join(audio_dir, noise_filename), sr=sample_rate)
 
 # Total duration = y/sr
-total_duration_clean = get_duration(y=y_clean, sr=sr_clean)
-total_duration_mixed = get_duration(y=y_mixed, sr=sr_mixed)
-total_duration_noise = get_duration(y=y_noise, sr=sr_noise)
+total_duration_clean = librosa.get_duration(y=y_clean, sr=sr_clean)
+total_duration_mixed = librosa.get_duration(y=y_mixed, sr=sr_mixed)
+total_duration_noise = librosa.get_duration(y=y_noise, sr=sr_noise)
 print("Duration clean: {} seconds.".format(total_duration_clean))
 print("Duration mixed: {} seconds.".format(total_duration_mixed))
 print("Duration noise: {} seconds.".format(total_duration_noise))
