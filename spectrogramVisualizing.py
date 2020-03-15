6bbf8af54906052981cf39d3d2f762f449145c20
betegon <miguelbetegongarcia@gmail.com>
2020-03-15 12:37:09 +0100

    Create dataset

--- spectrogramVisualizing.py
+++ spectrogramVisualizing.py
@@ -1,11 +1,12 @@
+from progress.bar import IncrementalBar
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-
+# TODO: Fill Inference audio to match size for STFT
 # TODO: Document That I have used zero padding for STFT
-# TODO: Change values in args.py to get 16064Hz as frame rate  and all the other parameters.
+# TODO: Change values in args.py to get 16128Hz as frame rate  and all the other parameters.
 # TODO: Rename audio_to_audio_frame_stack() to time_series_to_frame_stack()
 # TODO: Substitute the old functions by the ones here.
 # TODO: Start training from the weights provided in the repo.
@@ -16,6 +17,19 @@ import os
 #       Maybe increasing its amplitude. Check blend_noise_randomly()
 # TODO: Handle different encoding types of audio + channels (2 vs 1 channel).
 #       Best way is maybe convert from 2 to 1 channel.
+
+
+''''STEPS FOR CREATING DATASET
+1. Audio files to numpy (audio_files())
+    1.1. Load .wav
+    1.2. audio to audio frame stack (audio_to_audio_frame_stack())
+    1.3. Append result from 1.2 to audio list (list_sound_array)
+2. Blend noise randomly (blend_noise_randomly())
+3. noisy_voice_long = reshape
+4. save noisy_voice_long
+5. Repeat 3. & 4. for voice_long and noise_long
+'''
+
 
 
 def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length, hop_length_frame, min_duration):
@@ -38,7 +52,8 @@ def audio_files_to_numpy(audio_dir, list_audio_files, sample_rate, frame_length,
     list_audio_below_min_duration = []
 
     print("Total files to be processed: {}".format(len(list_audio_files)))
-    for file in list_audio_files:
+
+    for file in IncrementalBar('Audio file to numpy').iter(list_audio_files):
         # open the audio file
         y, sr = librosa.load(os.path.join(audio_dir, file), sr=sample_rate)
         total_duration = librosa.get_duration(y=y, sr=sr)
@@ -117,14 +132,12 @@ def numpy_audio_to_matrix_spectrogram(numpy_audio, dim_square_spec, n_fft, hop_l
     m_mag_db = np.zeros((nb_audio, dim_square_spec, dim_square_spec))
     m_phase = np.zeros((nb_audio, dim_square_spec, dim_square_spec), dtype=complex)
 
-    for i in range(nb_audio):
+    for i in IncrementalBar('Numpy audio to matrix spectrogram').iter(range(nb_audio)):
         m_mag_db[i, :, :], m_phase[i, :, :] = audio_to_magnitude_db_and_phase(
             n_fft, hop_length_fft, numpy_audio[i])
 
     print('\nShape of Spectrograms generated: {}\n'.format(m_mag_db.shape[1:]))
     return m_mag_db, m_phase
-
-
 
 
 def save_audio(y, sample_rate, output_name='audio_ouput.wav'):
@@ -136,6 +149,7 @@ def save_audio(y, sample_rate, output_name='audio_ouput.wav'):
         y (ndarray): shape should be (n,) or (2,n).
     '''
     librosa.output.write_wav(output_name, y, sample_rate)
+    print("Duration of written audio is: {} seconds.".format(librosa.get_duration(y=y, sr=sample_rate)))
 
 
 def plot_time_series(time_series_list, time_series_titles):
@@ -154,6 +168,13 @@ def plot_time_series(time_series_list, time_series_titles):
     plt.show()
 
 
+
+
+
+
+
+
+
 def main():
     audio_dir = 'spectrogramVisualizing/medium'
     clean_filename = 'clean.wav'
@@ -166,49 +187,39 @@ def main():
     n_fft = 512
     hop_length_fft = 63
 
-    path_save_spectrogram ='/home/betegon/Desktop/DENOISER/example/Speech-enhancement/Train/spectrogram/'
-    path_save_time_serie = '/home/betegon/Desktop/DENOISER/example/Speech-enhancement/Train/time_serie'
+    path_save_spectrogram   ='/home/betegon/Desktop/DENOISER/example/Speech-enhancement/Train/spectrogram'
+    path_save_time_serie    = '/home/betegon/Desktop/DENOISER/example/Speech-enhancement/Train/time_serie'
+    path_save_sound         ='/home/betegon/Desktop/DENOISER/example/Speech-enhancement/Train/sound'
+    path_train = "Train/finished_28spk_para_example/"
 
 
 
-
-    # Load .wav files to plot
-    y_clean, sr_clean = librosa.load(os.path.join(audio_dir, clean_filename), sr=sample_rate)
-    y_mixed, sr_mixed = librosa.load(os.path.join(audio_dir, mixed_filename), sr=sample_rate)
-    y_noise, sr_noise = librosa.load(os.path.join(audio_dir, noise_filename), sr=sample_rate)
-    # Total duration = y/sr
-    print("Duration clean: {} seconds.".format(librosa.get_duration(y=y_clean, sr=sr_clean)))
-    print("Duration mixed: {} seconds.".format(librosa.get_duration(y=y_mixed, sr=sr_mixed)))
-    print("Duration noise: {} seconds.".format(librosa.get_duration(y=y_noise, sr=sr_noise)))
-
-    # PLOT TIME SERIES
-    time_series = [y_clean, y_mixed, y_noise, y_mixed - y_noise]
-    titles = ['Clean voice', 'Mixed voice', 'Noise', 'Mixed voice - Noise = Clean voice']
-    plot_time_series(time_series, titles)
-
-    ''''STEPS FOR CREATING DATASET
-    1. Audio files to numpy (audio_files())
-        1.1. Load .wav
-        1.2. audio to audio frame stack (audio_to_audio_frame_stack())
-        1.3. Append result from 1.2 to audio list (list_sound_array)
-    2. Blend noise randomly (blend_noise_randomly())
-    3. noisy_voice_long = reshape
-    4. save noisy_voice_long
-    5. Repeat 3. & 4. for voice_long and noise_long
-    '''
+    audio_folders = sorted(os.listdir(path_train))  # get folder names
+    print(audio_folders)
+    clean_list = []
+    noise_list = []
+    for file_name in IncrementalBar('Processing').iter(audio_folders):
+        print("\n{}\n".format(file_name))
+        path_to_audio = path_train + file_name
+        path_to_clean = path_to_audio +'/clean.wav'
+        path_to_noise = path_to_audio + '/noise.wav'
+        print("path to audio: {}".format(path_to_audio))
+        print("path to clean: {}".format(path_to_clean))
+        print("path to noise: {}".format(path_to_noise))
+        clean_list.append(path_to_clean)
+        noise_list.append(path_to_noise)
 
     # 1. Audio files to numpy
-    audio_dir = 'spectrogramVisualizing/All_together'
-    clean1 = 'clean.wav'
-    clean2 = 'clean2.wav'
-    y_clean1, sr_clean1 = librosa.load(os.path.join(audio_dir, clean1), sr=sample_rate)
-    y_clean2, sr_clean2 = librosa.load(os.path.join(audio_dir, clean2), sr=sample_rate)
-    print("Duration clean1: {} seconds.".format(librosa.get_duration(y=y_clean1, sr=sr_clean1)))
-    print("Duration clean2: {} seconds.".format(librosa.get_duration(y=y_clean2, sr=sr_clean2)))
-
+    # audio_dir = 'spectrogramVisualizing/All_together'
+    audio_dir = ''
     # [START] AUDIO FILES TO NUMPY + SAVE LONG WAVES
-    clean_voice = audio_files_to_numpy(audio_dir, ['clean.wav', 'clean2.wav'], sample_rate, frame_length, hop_length_frame, min_duration)
-    noise = audio_files_to_numpy(audio_dir, ['noise.wav', 'noise2.wav'], sample_rate, frame_length, hop_length_frame, min_duration)
+    import glob
+    # clean_list = [os.path.basename(x) for x in sorted(glob.glob("{}/clean*".format(audio_dir)))]
+    # noise_list = [os.path.basename(x) for x in sorted(glob.glob("{}/noise*".format(audio_dir)))]
+    # print(clean_list)
+    # print(noise_list)
+    clean_voice = audio_files_to_numpy(audio_dir,clean_list[:300], sample_rate, frame_length, hop_length_frame, min_duration)
+    noise = audio_files_to_numpy(audio_dir, noise_list[:300], sample_rate, frame_length, hop_length_frame, min_duration)
     noisy = clean_voice + noise
     print("shape of clean_voice: {}".format(clean_voice.shape))
     print("shape of noisy: {}".format(noisy.shape))
